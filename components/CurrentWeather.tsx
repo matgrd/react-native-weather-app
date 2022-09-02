1de654c1556ba844d958dd6a1f7219c839aa4849
Mateusz Grden <matgrden@gmail.com>
2022-09-02 20:03:05 +0200

    switch for icons added and details changed

--- components/CurrentWeather.tsx
+++ components/CurrentWeather.tsx
@@ -27,7 +27,8 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   weatherIcon: {
-    width: 100,
+    width: 125,
+    height: 125,
   },
   bottom: {
     flexDirection: "row",
@@ -45,7 +46,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: "#fff",
     paddingLeft: 15,
-    paddingBottom: 15,
+    paddingBottom: 30,
     width: 150,
   },
   parameterRow: {
@@ -55,59 +56,115 @@ const styles = StyleSheet.create({
   },
   parameterLabel: {
     color: "#fff",
-    textAlign: "left",
+    fontSize: 14,
   },
   parameterValue: {
     color: "#fff",
+    fontSize: 14,
   },
 });
 
 // change data type
+// change imageSource type
 
 const CurrentWeather = ({ data }: any) => {
+  let imageSource: any = "";
+
+  switch (data.weather[0].icon) {
+    case "01d":
+      imageSource = require("../assets/weatherIcons/01d.png");
+      break;
+    case "01n":
+      imageSource = require("../assets/weatherIcons/01n.png");
+      break;
+    case "02d":
+      imageSource = require("../assets/weatherIcons/02d.png");
+      break;
+    case "02n":
+      imageSource = require("../assets/weatherIcons/02n.png");
+      break;
+    case "03d":
+      imageSource = require("../assets/weatherIcons/03d.png");
+      break;
+    case "03n":
+      imageSource = require("../assets/weatherIcons/03n.png");
+      break;
+    case "04d":
+      imageSource = require("../assets/weatherIcons/04d.png");
+      break;
+    case "04n":
+      imageSource = require("../assets/weatherIcons/04n.png");
+      break;
+    case "09d":
+      imageSource = require("../assets/weatherIcons/09d.png");
+      break;
+    case "09n":
+      imageSource = require("../assets/weatherIcons/09n.png");
+      break;
+    case "10d":
+      imageSource = require("../assets/weatherIcons/10d.png");
+      break;
+    case "11n":
+      imageSource = require("../assets/weatherIcons/11n.png");
+      break;
+    case "13d":
+      imageSource = require("../assets/weatherIcons/13d.png");
+      break;
+    case "13n":
+      imageSource = require("../assets/weatherIcons/13n.png");
+      break;
+    case "50d":
+      imageSource = require("../assets/weatherIcons/50d.png");
+      break;
+    case "50n":
+      imageSource = require("../assets/weatherIcons/50n.png");
+      break;
+    default:
+      imageSource = require("../assets/weatherIcons/04d.png");
+  }
+
   return (
-    <>
-      {data && (
-        <View style={styles.infoView}>
-          <View style={styles.top}>
-            <View>
-              <Text style={styles.cityCountryText}>
-                {`${data?.name}, ${data?.sys?.country}`}
-              </Text>
-              <Text style={styles.weatherDescription}>clear sky</Text>
-            </View>
-            <Image
-              style={styles.weatherIcon}
-              source={require("../assets/weatherIcons/01d.png")}
-            />
+    <View style={styles.infoView}>
+      <View style={styles.top}>
+        <View>
+          <Text style={styles.cityCountryText}>
+            {`${data.name}, ${data.sys.country}`}
+          </Text>
+          <Text style={styles.weatherDescription}>
+            {`${data.weather[0].description
+              .charAt(0)
+              .toUpperCase()}${data.weather[0].description.slice(1)}`}
+          </Text>
+        </View>
+        {imageSource !== "" ? (
+          <Image style={styles.weatherIcon} source={imageSource} />
+        ) : null}
+      </View>
+      <View style={styles.bottom}>
+        <Text style={styles.temperature}>{`${Math.round(data.main.temp)}`}°C</Text>
+        <View style={styles.details}>
+          <View style={styles.parameterRow}>
+            <Text style={styles.parameterLabel}>Details:</Text>
           </View>
-          <View style={styles.bottom}>
-            <Text style={styles.temperature}>27°C</Text>
-            <View style={styles.details}>
-              <View style={styles.parameterRow}>
-                <Text style={styles.parameterLabel}>Details:</Text>
-              </View>
-              <View style={styles.parameterRow}>
-                <Text style={styles.parameterLabel}>Feels like:</Text>
-                <Text style={styles.parameterValue}>22°C</Text>
-              </View>
-              <View style={styles.parameterRow}>
-                <Text style={styles.parameterLabel}>Wind:</Text>
-                <Text style={styles.parameterValue}>2.06 m/s</Text>
-              </View>
-              <View style={styles.parameterRow}>
-                <Text style={styles.parameterLabel}>Humidity:</Text>
-                <Text style={styles.parameterValue}>78%</Text>
-              </View>
-              <View style={styles.parameterRow}>
-                <Text style={styles.parameterLabel}>Pressure:</Text>
-                <Text style={styles.parameterValue}>1015 hPa</Text>
-              </View>
-            </View>
+          <View style={styles.parameterRow}>
+            <Text style={styles.parameterLabel}>Feels like:</Text>
+            <Text style={styles.parameterValue}>{Math.round(data.main.feels_like)}°C</Text>
+          </View>
+          <View style={styles.parameterRow}>
+            <Text style={styles.parameterLabel}>Wind:</Text>
+            <Text style={styles.parameterValue}>{data.wind.speed} m/s</Text>
+          </View>
+          <View style={styles.parameterRow}>
+            <Text style={styles.parameterLabel}>Humidity:</Text>
+            <Text style={styles.parameterValue}>{data.main.humidity}%</Text>
+          </View>
+          <View style={styles.parameterRow}>
+            <Text style={styles.parameterLabel}>Pressure:</Text>
+            <Text style={styles.parameterValue}>{data.main.pressure} hPa</Text>
           </View>
         </View>
-      )}
-    </>
+      </View>
+    </View>
   );
 };
 
