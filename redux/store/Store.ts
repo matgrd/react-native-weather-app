caacb9563cd45f778233bbfe28b9ac17c8cd3ac2
Mateusz Grden <matgrden@gmail.com>
2022-09-03 22:26:56 +0200

    redux store created

---
+++ redux/store/Store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+
+const reducer = combineReducers({});
+
+export const store = configureStore({ reducer });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
