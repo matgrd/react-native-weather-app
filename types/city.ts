099f858c42514cdd617f41562bddb5fba71b03b6
Mateusz Grden <matgrden@gmail.com>
2022-09-04 20:57:14 +0200

    City type added

---
+++ types/city.ts
@@ -0,0 +1,14 @@
+export type City = {
+  city: string;
+  country: string;
+  countryCode: string;
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  population: number;
+  region: string;
+  regionCode: string;
+  type: string;
+  wikiDataId: string;
+};
