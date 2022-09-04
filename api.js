8d8ef242a50983c7081306c8cf21d0e133992de9
Mateusz Grden <matgrden@gmail.com>
2022-09-04 21:01:10 +0200

    geoApiOptions, geoApiUrl added

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
-import openWeatherKey from "./config/env";
+import { openWeatherKey } from "./config/env";
+import { xRapidApiKey } from "./config/env";
 
 export const WEATHER_API_URL = {
   key: openWeatherKey,
   baseUrl: "https://api.openweathermap.org/data/2.5/",
 };
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": xRapidApiKey,
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
