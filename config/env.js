c9163a4cc4565b5b76838b379b1b6991b56b8969
Mateusz Grden <matgrden@gmail.com>
2022-09-06 00:17:02 +0200

    security added

--- config/env.js
+++ config/env.js
@@ -1,7 +1,9 @@
 import { X_RAPID_API_KEY } from "@env";
 import { OPEN_WEATHER } from "@env";
 
+if (!OPEN_WEATHER || !X_RAPID_API_KEY) {
+  throw new Error("Provide env variables");
+}
+
 export const openWeatherKey = OPEN_WEATHER;
 export const xRapidApiKey = X_RAPID_API_KEY;
-
-
