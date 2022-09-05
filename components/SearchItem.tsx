69a43e44463223af3462b5a543ad2258a755cf2e
Mateusz Grden <matgrden@gmail.com>
2022-09-05 18:58:24 +0200

    added styles using StyleSheet

--- components/SearchItem.tsx
+++ components/SearchItem.tsx
@@ -1,18 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { City } from "../types/city";
 
-const SearchItem = (item: City) => {
+const styles = StyleSheet.create({
+  box: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+  },
+  item: {
+    marginLeft: 10,
+    flexShrink: 1,
+  },
+  name: {
+    fontFamily: "RobotoBold",
+  },
+  country: {
+    fontSize: 12,
+    fontFamily: "RobotoRegular",
+  },
+});
+
+const SearchItem = ({ item }: City) => {
   return (
-    <View style={{ flexDirection: "row", alignItems: "center", padding: 15 }}>
+    <View style={styles.box}>
       <MaterialIcons
         name={item.type === "CITY" ? "location-city" : "location-on"}
         color={"black"}
         size={30}
       />
-      <View style={{ marginLeft: 10, flexShrink: 1 }}>
-        <Text style={{ fontWeight: "700" }}>{item.name}</Text>
-        <Text style={{ fontSize: 12 }}>{item.country}</Text>
+      <View style={styles.item}>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.country}>{item.country}</Text>
       </View>
     </View>
   );
