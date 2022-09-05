fc6a1b403d75b7617ff5b6ae00cade8a908a7f8f
Mateusz Grden <matgrden@gmail.com>
2022-09-06 00:16:15 +0200

    latitude, longitude types changed state type added

--- redux/slices/geographicalCoordinatesSlice.tsx
+++ redux/slices/geographicalCoordinatesSlice.tsx
@@ -2,14 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface GeographicalCoordinates {
-  latitude: string;
-  longitude: string;
+  latitude: number;
+  longitude: number;
   status: boolean;
 }
 
 const initialState: GeographicalCoordinates = {
-  latitude: "0",
-  longitude: "0",
+  latitude: 0,
+  longitude: 0,
   status: false,
 };
 
@@ -17,13 +17,22 @@ export const geographicalCoordinatesSlice = createSlice({
   name: "geographicalCoordinates",
   initialState,
   reducers: {
-    setLatitude: (state: any, action: PayloadAction<string>) => {
+    setLatitude: (
+      state: GeographicalCoordinates,
+      action: PayloadAction<number>
+    ) => {
       state.latitude = action.payload;
     },
-    setLongitude: (state: any, action: PayloadAction<string>) => {
+    setLongitude: (
+      state: GeographicalCoordinates,
+      action: PayloadAction<number>
+    ) => {
       state.longitude = action.payload;
     },
-    setStatus: (state: any, action: PayloadAction<boolean>) => {
+    setStatus: (
+      state: GeographicalCoordinates,
+      action: PayloadAction<boolean>
+    ) => {
       state.status = action.payload;
     },
   },
